5c15e5b79fa481b59a95e62fc5a4d2af8767a184
root <root@ip-172-31-46-173.eu-north-1.compute.internal>
2023-12-20 04:41:30 +0000

    commited

---
+++ add.java
@@ -0,0 +1,8 @@
+class Add{
+    public static void main(String[] args){
+        add(4,5)
+    }
+    public void add(int a,int b){
+        System.out.println("summ is "+(a+b));
+    }
+}
---
+++ mul.java
@@ -0,0 +1,8 @@
+class Add{
+    public static void main(String[] args){
+        mul(4,5)
+    }
+    public void mul(int a,int b){
+        System.out.println("multiplication is "+(a*b));
+    }
+}
