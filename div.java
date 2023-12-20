11f45838c0787b1bcfe723b6316139b4307ebe07
ex135 <ex135@win2k18.local>
2023-12-20 10:14:33 +0530

    commited

---
+++ div.java
@@ -0,0 +1,8 @@
+class div{
+    public static void main(String[] args){
+        div(4,5)
+    }
+    public void div(int a,int b){
+        System.out.println("division is "+(a/b));
+    }
+}
---
+++ sub.java
@@ -0,0 +1,8 @@
+class sub{
+    public static void main(String[] args){
+        sub(4,5)
+    }
+    public void div(int a,int b){
+        System.out.println("sub is "+(a-b));
+    }
+}
